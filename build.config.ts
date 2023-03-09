dda95c5efeccb08fbc5af6d752c21d05fea0c298
ahacad <ahacad@126.com>
2023-03-09 15:09:32 +0800

    chore: add build.config.ts for unbuild

---
+++ build.config.ts
@@ -0,0 +1,16 @@
+import { defineBuildConfig } from "unbuild";
+import fg from "fast-glob";
+
+export default defineBuildConfig({
+  entries: [
+    ...fg
+      .sync("src/**/*.ts", { ignore: ["src/**/*.test.ts"] })
+      .map((i) => i.slice(0, -3)),
+  ],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+  },
+});
